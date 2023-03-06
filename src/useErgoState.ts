767932ea88dd58a8a0d72ff79abd0648a7ab2e76
Ethan Standel <ethan@standel.io>
2023-03-05 21:46:01 -0500

    fix: make the value returned by useErgoState be stable, and add simple output types

--- src/useErgoState.ts
+++ src/useErgoState.ts
@@ -1,34 +1,43 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback } from "react";
 
-type Setter<S> = ((newState: S) => S) | (S extends Function ? never : S);
-type OptionalSetter<S = undefined> = S extends undefined
+export type MutableRequiredStateRef<S> = (setStateAction?: Setter<S>) => S;
+export type MutableOptionalStateRef<S> = (
+  setStateAction?: OptionalSetter<S | undefined>
+) => S | undefined;
+export type MutableStateRef<S> =
+  | MutableRequiredStateRef<S>
+  | MutableOptionalStateRef<S>;
+export type Setter<S> = ((newState: S) => S) | (S extends Function ? never : S);
+export type OptionalSetter<S = undefined> = S extends undefined
   ? never
   : ((newState: S | undefined) => S | undefined) | S;
-function useErgoState<S = undefined>(): (
-  setStateAction?: OptionalSetter<S>
-) => S | undefined;
+
 function useErgoState<S>(
   initialState: S | (() => S)
-): (setStateAction?: Setter<S>) => S;
-function useErgoState<S>(
-  initialState?: S | (() => S)
-): (setStateAction?: OptionalSetter<S>) => S | undefined {
+): MutableRequiredStateRef<S>;
+function useErgoState<S = undefined>(): (
+  setStateAction?: OptionalSetter<S>
+) => MutableOptionalStateRef<S>;
+function useErgoState<S>(initialState?: S | (() => S)): MutableStateRef<S> {
   const [state, setState] = useState(initialState);
   const stateRef = useRef(state);
 
-  return (setStateAction?: OptionalSetter<S>): S | undefined => {
-    if (setStateAction !== undefined) {
-      const newStateValue =
-        typeof setStateAction === "function"
-          ? setStateAction(stateRef.current)
-          : setStateAction;
-      stateRef.current = newStateValue;
-      setState(newStateValue);
-      return newStateValue;
-    }
+  return useCallback(
+    (setStateAction?: OptionalSetter<S | undefined>): S | undefined => {
+      if (setStateAction !== undefined) {
+        const newStateValue =
+          typeof setStateAction === "function"
+            ? setStateAction(stateRef.current)
+            : setStateAction;
+        stateRef.current = newStateValue;
+        setState(newStateValue);
+        return newStateValue;
+      }
 
-    return stateRef.current;
-  };
+      return stateRef.current;
+    },
+    []
+  );
 }
 
 export { useErgoState, useErgoState as useState };
